5f15d79be067955745d754a8aef84d699e5a8e40
hanlie <johansama9999@gmail.com>
2024-12-04 09:56:02 +0700

    Initial commit

---
+++ src/main/java/com/shopifyproject/project/ShopifyAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shopifyproject.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifyAppProjectApplication.class, args);
+	}
+
+}
